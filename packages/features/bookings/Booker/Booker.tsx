538b0befd964ab6597c204ed51a91ba57658e08c
ARK88\bill.liu <bill.liu@ark88.net>
2026-01-14 10:10:52 +0800

    1.3.0

--- packages/features/bookings/Booker/Booker.tsx
+++ packages/features/bookings/Booker/Booker.tsx
@@ -92,6 +92,7 @@ const BookerComponent = ({
   WrappedBookerProps & {
     thresholdJson?: string;
   }) => {
+  console.log("version 1.3.0")
   const { t, i18n } = useLocale();
   const searchParams = useCompatSearchParams();
   const isPlatformBookerEmbed = useIsPlatformBookerEmbed();
