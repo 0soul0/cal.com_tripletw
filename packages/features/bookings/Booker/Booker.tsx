e453d0030e7807a31da7f7af687c28bf4a3b0f67
bill <bill@gmail.com>
2026-01-16 01:58:38 +0800

    1.3.2

--- packages/features/bookings/Booker/Booker.tsx
+++ packages/features/bookings/Booker/Booker.tsx
@@ -92,7 +92,7 @@ const BookerComponent = ({
   WrappedBookerProps & {
     thresholdJson?: string;
   }) => {
-  console.log("version 1.3.1")
+  console.log("version 1.3.2")
   const { t, i18n } = useLocale();
   const searchParams = useCompatSearchParams();
   const isPlatformBookerEmbed = useIsPlatformBookerEmbed();
