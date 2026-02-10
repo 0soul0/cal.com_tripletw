ce6bfcf4c6de7f8f32c80ea918179822e2346de2
bill <bill@gmail.com>
2026-02-10 21:32:58 +0800

    add url of gas in dockerfile

--- apps/web/app/(booking-page-wrapper)/[user]/[type]/page.tsx
+++ apps/web/app/(booking-page-wrapper)/[user]/[type]/page.tsx
@@ -73,7 +73,7 @@ async function getThresholdData(scheduleId: number | undefined) {
 }
 
 const ServerPage = async ({ params, searchParams }: PageProps) => {
-  console.log("version 1.3.4")
+  console.log("version 1.3.5")
   const decodedParams = decodeParams(await params);
   const searchParamsValue = await searchParams;
   const searchParamsString = new URLSearchParams(searchParamsValue as any).toString();
--- packages/features/bookings/Booker/Booker.tsx
+++ packages/features/bookings/Booker/Booker.tsx
@@ -92,7 +92,7 @@ const BookerComponent = ({
   WrappedBookerProps & {
     thresholdJson?: string;
   }) => {
-  console.log("version 1.3.4")
+  console.log("version 1.3.5")
   const { t, i18n } = useLocale();
   const searchParams = useCompatSearchParams();
   const isPlatformBookerEmbed = useIsPlatformBookerEmbed();
